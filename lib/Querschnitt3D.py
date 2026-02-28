90db4ff1ee5de208ef8def5b6897fe0ac7377f66
retep <retep@retep>
2026-02-28 15:31:55 +0100

    reduce Security items

--- lib/Querschnitt3D.py
+++ lib/Querschnitt3D.py
@@ -1,11 +1,8 @@
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-from matplotlib import cm
 import numpy as np
 from .Layer import DammL, IntL
 from .Raster import Raster
-from qgis.core import *
-from matplotlib import colormaps
+from qgis.core import QgsPoint,QgsPointXY
 from qgis.utils import iface
 from qgis.gui import QgsVertexMarker
 from qgis.PyQt.QtGui import QColor
@@ -136,53 +133,3 @@ class Querschnitt3D:
             m.setFillColor(QColor(200, 0, 0))
         if col == 4:
             m.setFillColor(QColor(0, 200, 200))
-
-
-"""
-        X, Y = np.meshgrid(X1, Y1)
-        for x in range(int(llx)):
-            for y in range( int(llx)):
-                X[x][y]=px.x()-dx/2*x
-                Y[x][y]=px.y()-dy/2*y
-"""
-
-
-"""
-        plt.style.use('_mpl-gallery')
-        # Make data
-       
-        X, Y = np.meshgrid(X1, Y1)
-        Z=X.copy()
-        for x in range(100):
-            for y in range( 100):
-                z=self.raster.getValue(QgsPoint(X[x][y],Y[x][y]))
-                Z[x][y]=z   
-
-        la=[]
-        ra=[]
-        for x in range(8):    
-            p1,p2=self.inten.getPlPr(laenge-40+x*10)
-            la.append(p1)
-            ra.append(p2)
-        v=[]
-        v2=[]
-        for p in la:
-            v.append([p.x(),p.y(),p.z()])
-            v2.append([p.x(),p.y(),p.m()])
-        ra.reverse()
-        for p in ra:
-            v.append([p.x(),p.y(),p.z()])
-            v2.append([p.x(),p.y(),p.m()])
-        
-        quadrat = [v]
-        quadrat2 = [v2]
-
-        # Plot the surface
-        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-        ax.plot_surface(X, Y, Z,alpha=0.3,color='green',label='Gelaende',edgecolors='gray',linewidth=0.5)
-        p3dc = Poly3DCollection(quadrat, alpha=0.6, facecolors =['blue'], edgecolors=['blue'],label='Quote Abfluss')
-        ax.add_collection3d(p3dc)
-        p3dc2 = Poly3DCollection(quadrat2, alpha=0.6,facecolors =['red'], edgecolors=['red'],label='Energielinienhoehe')
-        ax.add_collection3d(p3dc2)
-        plt.show()
-"""
